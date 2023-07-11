02f577ad200509f6e26dff6c7d5828d4179069c4
OLT2KOR <130150803+OLT2KOR@users.noreply.github.com>
2023-07-11 15:52:16 +0530

    Changed

--- src/main/java/com/example/myapp/MyApp.java
+++ src/main/java/com/example/myapp/MyApp.java
@@ -2,6 +2,6 @@ package com.example.myapp;
 
 public class MyApp {
     public static void main(String[] args) {
-        System.out.println("Hello, This is my first Azure Pipeline");
+        System.out.println("Hello, This is first Azure Pipeline");
     }
 }
